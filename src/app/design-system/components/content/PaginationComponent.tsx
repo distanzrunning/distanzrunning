5943413e30ab10e71d75822028f8e43b99140278
Robert Clements <robertclements@Mac.lan>
2026-01-25 17:37:10 +0100

    Add hover states to pagination (chevron and label go black/white on hover)

--- src/app/design-system/components/content/PaginationComponent.tsx
+++ src/app/design-system/components/content/PaginationComponent.tsx
@@ -389,11 +389,11 @@ export default function PaginationComponent() {
                   aria-label="Go to previous page: Button"
                   className="group flex items-end text-left"
                 >
-                  <span className="text-[var(--ds-gray-1000)] mb-0.5">
+                  <span className="text-[var(--ds-gray-1000)] group-hover:text-black dark:group-hover:text-white transition-colors mb-0.5">
                     <ChevronLeftIcon />
                   </span>
                   <div className="flex flex-col items-start ml-1">
-                    <span className="text-[0.8125rem] leading-[1.125rem] font-normal text-[var(--ds-gray-1000)] mb-1">
+                    <span className="text-[0.8125rem] leading-[1.125rem] font-normal text-[var(--ds-gray-1000)] group-hover:text-black dark:group-hover:text-white transition-colors mb-1">
                       Previous
                     </span>
                     <span className="text-[1rem] leading-[1.5rem] font-medium text-[var(--ds-gray-1000)]">
@@ -409,14 +409,14 @@ export default function PaginationComponent() {
                   className="group flex items-end text-right"
                 >
                   <div className="flex flex-col items-start mr-1">
-                    <span className="text-[0.8125rem] leading-[1.125rem] font-normal text-[var(--ds-gray-1000)] mb-1">
+                    <span className="text-[0.8125rem] leading-[1.125rem] font-normal text-[var(--ds-gray-1000)] group-hover:text-black dark:group-hover:text-white transition-colors mb-1">
                       Next
                     </span>
                     <span className="text-[1rem] leading-[1.5rem] font-medium text-[var(--ds-gray-1000)]">
                       Code Block
                     </span>
                   </div>
-                  <span className="text-[var(--ds-gray-1000)] mb-0.5">
+                  <span className="text-[var(--ds-gray-1000)] group-hover:text-black dark:group-hover:text-white transition-colors mb-0.5">
                     <ChevronRightIcon />
                   </span>
                 </button>
