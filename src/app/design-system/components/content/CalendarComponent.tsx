b029744a6333c64f81e63282aa26899a9bd4b1b0
Robert Clements <robertclements@Mac.lan>
2026-01-26 22:58:39 +0100

    Fix timezone select: auto-width to fit content, centered container

--- src/app/design-system/components/content/CalendarComponent.tsx
+++ src/app/design-system/components/content/CalendarComponent.tsx
@@ -1260,7 +1260,7 @@ export default function CalendarComponent() {
                             </div>
 
                             {/* Timezone Selector */}
-                            <div className="mt-2 flex justify-center pl-4">
+                            <div className="mt-2 flex justify-center">
                               <label
                                 className="calendar-timezone-label"
                                 data-version="v1"
